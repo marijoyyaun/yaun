9913d1e952a3352c6b355b4d34bd47ece63ce660
marijoyyaun <41813293+marijoyyaun@users.noreply.github.com>
2018-07-28 13:01:54 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>My Personal Profile</title> 
+
+
+
+</head>
+
+<body>
+
+<center>
+
+<h1>My Personal Profile</h1>
+
+<table>
+
+<tr><td  width = 300px>
+
+<h2>Name         : Marijoy M. Yaun </h2>
+
+<h3>Address      : B1 L11 Raysland Subdivision, Tayud, Liloan</h3>
+
+<h5>Mobile Number: 09189594310</h5>
+
+<h4>Email Address: joyyaun@gmail.com</h4>
+
+<h6>Birthday     : 10-14-1969</h6></td>
+
+<td width=200px><img src="images/woman.jpg" style="width:150px;height:150px;"/></td>
+
+</tr>
+
+
+
+<tr>
+
+<td>
+
+<h2> Goal:</h2><p> <strong>Seeking a challenging career </strong>with a progressive organization that provides an opportunity to capitalize my technical skills in the field of Information Technology.
+
+</td>
+
+</tr>
+
+<tr>
+
+<td>
+
+<h2> Objectives:</h2><ul> <li>To be a part of an organization where I can <i>fully utilize my skills </i>and make a significant contribution to the success of the employer and at the same time my individual growth.</li>
+
+<li>To have a growth oriented and challenging career, where I can contribute my knowledge and skills to the organization and enhance my experience through continuous learning and teamwork.</li>
+
+</td>
+
+</table></center>
+
+
+
+
+
+</body>
+
+</html>
+
