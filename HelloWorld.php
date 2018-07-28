a6fa6dff62cb11fa403afbb42db64bf8db83f94c
marijoyyaun <41813293+marijoyyaun@users.noreply.github.com>
2018-07-28 13:47:54 +0800

    Add files via upload

---
+++ HelloWorld.php
@@ -0,0 +1,10 @@
+<h1>Hello Worl</h1>
+<p>
+<?php
+	echo "hi there, \n";
+	$answer = 6 * 7;
+	echo "The answer is $answer, whar was the question again?\n;
+?>
+</p>
+<p>
+<p>Yes another paragraph.</p>
